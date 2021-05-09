8c0bfdc4f1465bfcce52ec9d6684d22ffe0533fa
Thomas.Cornes <Thomas.cornes@my.jcu.edu.au>
2021-05-09 17:24:08 +1000

    Car.py commit of Class Car

---
+++ Car.py
@@ -0,0 +1,11 @@
+class Car:
+    """Represent a Car object."""
+
+    def __init__(self, name="", fuel=0):
+        """Initialise a Car instance.
+        name: string, reference name for car
+        fuel: float, one unit of fuel drives one kilometre
+        """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
