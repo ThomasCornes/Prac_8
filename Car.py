7ae090de44f0c00c6782d04c323ae6c7df5570c9
Thomas.Cornes <Thomas.cornes@my.jcu.edu.au>
2021-05-09 20:24:25 +1000

    Car.py Final Commit

--- Car.py
+++ Car.py
@@ -1,11 +1,26 @@
 class Car:
-    """Represent a Car object."""
+
 
     def __init__(self, name="", fuel=0):
-        """Initialise a Car instance.
-        name: string, reference name for car
-        fuel: float, one unit of fuel drives one kilometre
-        """
         self.name = name
         self.fuel = fuel
         self.odometer = 0
+
+    def __str__(self):
+
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
+                                                 self.odometer)
+
+    def add_fuel(self, amount):
+
+        self.fuel += amount
+
+    def drive(self, distance):
+
+        if distance > self.fuel:
+            distance = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+        self.odometer += distance
+        return distance
